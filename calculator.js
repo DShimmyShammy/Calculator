86f3e4aba851708d5ccafd704474abbd8778388a
Dhruv Saran <mac6@129mac18.valleychristian.net>
2017-01-06 10:53:04 -0800

    Javascript Id's

--- calculator.js
+++ calculator.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+// Change Display
+function d(val) {
+		document.getElementById("d").value = val;
+}
+
+// Type numbers and operators
+function v(val) {
+		docuemtn.getElementById("d").value += val;
+}
+
+// Evaluate the equation
+function e( )  {
+}
